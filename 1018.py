49d86c36aa89dcb5bf12ad18364e16d1ede51ca0
Vicente Couto Marczewski <vicentemarczewski@hotmail.com>
2021-07-21 20:15:02 -0300

    1018

--- 1018.py
+++ 1018.py
@@ -1,13 +1,18 @@
 #URI 1018 https://www.urionlinejudge.com.br/judge/pt/problems/view/1018
 
 N = int(input())
-while N > 1000000: #enquanto
+while N > 1000000 or N<0:
     print("digite novamente")
     N = int(input())
 
 notas = [100, 50, 20, 10, 5, 2, 1]
 
-#for i in notas:
+print(N)
+for i in notas: 
+    valor = N // i
+    print(f"{valor} nota(s) de R$ {i},00")
+    N = N%i
+
 
 
 
